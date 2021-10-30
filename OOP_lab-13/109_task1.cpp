662f5c47353105001407d3da57535547532fc197
NHAlvee <73606235+NHAlvee@users.noreply.github.com>
2021-10-30 17:06:46 +0600

    Create 109_task1.cpp

---
+++ OOP_lab-13/109_task1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[10];
+    set<int> s;
+    int j=0;
+    for(int i=0; i<20; i++)
+    {
+        if(i%2==0)
+        {
+            a[j]=i;
+            j++;
+        }
+        else s.insert(i);
+    }
+
+    vector<int> v(20);
+    merge(a,a+10,s.begin(),s.end(),v.begin());
+    for(auto i:v)
+        cout << i << ' ';
+
+}
