bcc9a16195a98a3d261a3b4a301374d570b06d6e
NHAlvee <73606235+NHAlvee@users.noreply.github.com>
2021-10-30 17:06:49 +0600

    Create 109_task2.cpp

---
+++ OOP_lab-13/109_task2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+namespace my_namespace
+{
+    double pi = 3.14;
+}
+int main()
+{
+    double pi = 3.15;
+    cout << pi << endl;
+    cout << my_namespace::pi;
+}
